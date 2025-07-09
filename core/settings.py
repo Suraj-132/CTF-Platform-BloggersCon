c1d425ff8874d5a78b962f5f38e7777e70fcf8d4
Suchitra Hiregowda <surajhiregowda2021@gmail.com>
2025-07-09 13:29:09 +0530

    Setup SMTP email backend for production

--- core/settings.py
+++ core/settings.py
@@ -43,8 +43,7 @@ INSTALLED_APPS = [
     'users',
     'rest_framework',
     'Challenge',
-    'teams',
-    
+    'teams',    
 ]
 
 AUTH_USER_MODEL = 'users.User'
@@ -66,7 +65,16 @@ SIMPLE_JWT = {
     'BLACKLIST_AFTER_ROTATION': True,
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_SSL = config('EMAIL_USE_SSL', cast=bool)
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
+
 
 
 
